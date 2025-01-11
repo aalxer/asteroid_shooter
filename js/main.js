fd67a4b2109779b97e80b577305e75a9ed1eda68
Ahmed Alexander <92260103+aalxer@users.noreply.github.com>
2025-01-11 18:21:47 +0100

    Code Documentation

--- js/main.js
+++ js/main.js
@@ -193,7 +193,7 @@ spaceshipMtlLoader.load('../assets/RaiderStarship.mtl', (materials) => {
 })
 
 // ---------------------------------------------------------------------------------------------------------------------
-// ENEMIES OBJECTS (Blade-Object)
+// ENEMIES OBJECTS (Blade)
 // ---------------------------------------------------------------------------------------------------------------------
 
 const enemyObjLoader = new OBJLoader();
@@ -483,7 +483,7 @@ function animateMissiles() {
 // ---------------------------------------------------------------------------------------------------------------------
 
 const speed = 0.005;
-const kollisionCheckingPosition = spaceshipYPos + 50;
+const kollisionCheckingPosition = spaceshipYPos + 50; // ab welche y-Position soll das kollisionChecking beginnen
 let isGameOver = false;
 let timer = 0;
 let killsCounter = 0;
@@ -493,55 +493,87 @@ let coinsList = [];
 let enemyShotsList = [];
 let missileObjects = [];
 
+/**
+ * Aktualisiert den CoinsCounter in Display
+ */
 function updateScore() {
     let element = document.getElementById('coinsState');
     element.innerText = coinsCounter;
 }
 
+/**
+ * Bewegt das Zielobjekt horizontal basierend auf der Tastatureingabe
+ * @param event: Das Tastaturereignis
+ * @param target: Das Zielobjekt, dessen Position geändert wird
+ */
 function moveUsingKeyboard(event, target) {
 
     if (!spaceshipControlsEnabled) return
 
     const moveSpeed = 10;
+
     if (target) {
         if (event.key === 'ArrowLeft') {
             target.position.x -= moveSpeed;
         } else if (event.key === 'ArrowRight') {
             target.position.x += moveSpeed;
         }
+        // Zielobjektposition einschränken, damit er das Display nicht verlässt:
         constrainObjectPosition(target);
     }
 }
 
+/**
+ * Bewegt ein Zielobjekt basierend auf der Mausposition
+ * @param event: Das Tastaturereignis
+ * @param target: Das Zielobjekt, dessen Position geändert wird
+ */
 function moveUsingMaus(event, target) {
 
     if (!spaceshipControlsEnabled) return
 
+    // Mausposition auf der x-Achse in -1 oder 1 umwandeln:
     let mouseX = 0;
     mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+
     if (target) {
+        // berechnete X-Mausposition auf die Spielfeldbreite skalieren,
+        // um die tatsächliche X-Koordinate des Zielobjekts ui bestimmen:
         target.position.x = mouseX * fieldWidth / 2;
+        // Zielobjektposition einschränken, damit er das Display nicht verlässt:
         constrainObjectPosition(target);
     }
 }
 
+/**
+ * Löst Sch+sse vom Raumschiff aus
+ * @param spaceship: Objekt, aus dem Schüsse ausgelöst werden
+ */
 function shoot(spaceship) {
 
+    // Schuss erstellen:
     const color = new THREE.Color(0xff9e13);
-    const shot = createBullet(color, 18);
+    const shot = createBullet(color, 15);
     shot.position.set(spaceship.position.x, spaceship.position.y, spaceship.position.z);
+
+    // animierter Feuereffekt anwenden:
     getFireEffect(shot);
+
     scene.add(shot);
 
+    // Schuss nach oben bewegen:
     const animateShot = () => {
 
         shot.position.y += 10;
+
+        // Schuss weiter bewegen, wenn er im sichtbaren Bereich ist, sonst löschen:
         if (shot.position.y < 1000) {
             requestAnimationFrame(animateShot);
         } else {
             scene.remove(shot);
         }
 
+        // Kollision an jeder Stelle mit Gegnern überprüfen:
         for (let i = 0; i < enemiesList.length; i++) {
             let enemy = enemiesList[i];
 
@@ -557,21 +589,31 @@ function shoot(spaceship) {
 
     };
 
+    // Animation des Schusses starten:
     animateShot();
 }
 
+/**
+ * Löst Schüsse vom einem Blade aus
+ * @param blade: Objekt, aus dem Schüsse ausgelöst werden
+ */
 function enemyShoot(blade) {
 
-    const color = new THREE.Color(0x3dbcff);
-    const shot = createBullet(color, 10);
     const startShootPosition = (fieldHeight / 2) - 50;
+
+    // Schuss erstellen:
+    const color = new THREE.Color(0x3dbcff);
+    const shot = createBullet(color, 7);
     shot.position.set(blade.position.x, blade.position.y, blade.position.z);
+    // Laser-Effekt an Schuss anwenden:
     let laser = getLaserEffect(shot);
     blade.add(shot);
     scene.add(shot);
 
+    // Schuss nach unten bewegen:
     const animateShot = () => {
 
+        // Kollision mit dem Raumschiff überprüfen:
         if (!isGameOver && shot.position.y <= kollisionCheckingPosition && checkCollision(spaceshipObj, shot)) {
             console.log("Spaceship was shot by a Blade");
             handleGameOver();
@@ -579,6 +621,7 @@ function enemyShoot(blade) {
 
         shot.position.y -= 10;
 
+        // Schuss weiter bewegen, wenn er im sichtbaren Bereich ist, sonst löschen:
         if (shot.position.y > (-fieldHeight / 2) && blade.position.y < startShootPosition) {
             requestAnimationFrame(animateShot);
 
@@ -589,9 +632,13 @@ function enemyShoot(blade) {
         }
     }
 
+    // Animation des Schusses starten:
     animateShot();
 }
 
+/**
+ * handelt GameOver
+ */
 function handleGameOver() {
 
     if (isGameOver) return
@@ -599,16 +646,11 @@ function handleGameOver() {
     console.log("Game Over !");
     isGameOver = true;
     spaceshipControlsEnabled = false;
+
+    // Explosionseffekt an Spaceship anwenden:
     getExplosionEffect(spaceshipObj);
-    /*
-    spaceshipObj.traverse((child) => {
-        if (child.isMesh) {
-            child.material.color.setHex(0x412200);
-        }
-    });
 
-     */
-
+    // nach 3s wird die GameOver-Page angezeigt:
     setTimeout(() => {
 
         console.log("Cleaning Scene and display GameOver-Page .. ");
@@ -616,13 +658,14 @@ function handleGameOver() {
         scene.remove(spaceshipObj);
         spaceshipObj = null;
 
-        // GUI GameOver Page:
+        // GameOver-Page einblenden::
         const statebarContainer = document.getElementById('statebarContainer');
         statebarContainer.classList.add('hideContainer');
 
         const gameOverContainer = document.getElementById('gameOverContainer');
         gameOverContainer.classList.add('displayContainer');
 
+        // Punktestand, Münzen und Kills anzeigen:
         const scoreElement = document.getElementById('scoreGameOverPage');
         const coinsElement = document.getElementById('coinsGameOverPage');
         const killsElement = document.getElementById('killsGameOverPage');
@@ -633,14 +676,21 @@ function handleGameOver() {
     }, 3000);
 }
 
+/**
+ * Beschränkt die Position eines Objekts auf Spielfeldgrenzen
+ * @param object: Das Objekt deren Position eingeschränkt werden soll
+ */
 function constrainObjectPosition(object) {
 
     if (object) {
+        // BoundingBox des Objekts berechnen:
         const boundingBox = new THREE.Box3().setFromObject(object);
         const width = boundingBox.max.x - boundingBox.min.x;
+        // Grenzen auf Basis der Fensterbreite berechnen
         const minX = -window.innerWidth / 2 + width / 2;
         const maxX = window.innerWidth / 2 - width / 2;
 
+        // Position auf die berechneten Grenzen beschränken:
         if (object.position.x < minX) {
             object.position.x = minX;
         } else if (object.position.x > maxX) {
@@ -649,14 +699,26 @@ function constrainObjectPosition(object) {
     }
 }
 
+/**
+ * Prüft, ob zwei Objekte kollidieren
+ * @returns ein Boolean, ob sich die Objekte überschneiden
+ */
 function checkCollision(obj1, obj2) {
 
+    // BoundingBoxen für beide Objekte erstellen:
     const box1 = new THREE.Box3().setFromObject(obj1);
     const box2 = new THREE.Box3().setFromObject(obj2);
 
+    // Prüfen ob sie sich überschneiden:
     return box1.intersectsBox(box2);
 }
 
+/**
+ * Erstellt ein Schuss mit bestimmter Farbe und Größe
+ * @param color: Farbe der Kugel
+ * @param size: Größe der Kugel
+ * @returns ein Kugel-Objekt
+ */
 function createBullet(color, size) {
 
     const sphereGeometry = new THREE.SphereGeometry(
@@ -676,6 +738,9 @@ function createBullet(color, size) {
     return sphere;
 }
 
+/**
+ * Startet das Bewegen vom Hintergrund auf y-achse
+ */
 function startAnimateBackground() {
 
     bgPlaneTextureMap.offset.y += speed;
@@ -684,13 +749,18 @@ function startAnimateBackground() {
         bgPlaneTextureMap.offset.x = 0;
     }
 
+    // Szene rendern und Animation wiederholen:
     renderer.render(scene, camera);
     requestAnimationFrame(startAnimateBackground);
 }
 
+/**
+ * Startet den Timer in einem Interval und zeigt den auf dem Display an
+ */
 const startTimer = () => {
     const interval = setInterval(() => {
 
+        // Timer stoppen, wenn das Spiel vorbei ist:
         if (isGameOver) {
             clearInterval(interval);
             console.log('Timer gestoppt, is GameOver');
@@ -703,6 +773,9 @@ const startTimer = () => {
 
 };
 
+/**
+ * Startet die Erstellung von Münzen in einem bestimmten Intervall
+ */
 function startCoinsCreation() {
     console.log("Coins Creation started .. ");
     setInterval(() => {
@@ -710,6 +783,9 @@ function startCoinsCreation() {
     }, Math.random() * 300 + 1000);
 }
 
+/**
+ * Startet die Erstellung von Gegner-Objekten in einem bestimmten Intervall
+ */
 function startEnemiesCreation() {
     console.log("Enemies Creation started .. ");
     setInterval(() => {
@@ -717,6 +793,9 @@ function startEnemiesCreation() {
     }, 5000);
 }
 
+/**
+ * Startet die Erstellung von Raketen in einem bestimmten Intervall
+ */
 function startMissilesCreation() {
     console.log("Missiles Creation started .. ");
     setInterval(() => {
@@ -732,98 +811,144 @@ startMissilesCreation();
 
 // ---------------------------------------------------------------------------------------------------------------------
 // PARTICLE SYSTEM
+// Zur Erzeugung animierter Effekte im Spiel, wie Rauch- und Explosionseffekte.
+// Jedes Partikelsystem ist in einem separaten Modul implementiert, um sie unabhängig flexibel voneinander anzupassen.
+// Die Module befinden sich in ./libs/ und basieren auf einem ursprünglich aus einem externen Git-Repository bezogenen Modul,
+// das speziell für verschiedene Effekte wie Rauch, Feuer, Laser und Explosionen angepasst wurde.
 // ---------------------------------------------------------------------------------------------------------------------
 
 function getSmokeEffect(object) {
 
+    // Kontext für das Partikelsystem definieren:
     let smokeEffectContext = {
-        camera,
-        emitter: object,
-        parent: scene,
-        rate: 8,
-        texture: '../assets/img/smoke.png'
+        camera, // Kamera, die die Partikel zeigt
+        emitter: object, // Objekt, das den Rauch ausstößt
+        parent: scene, // Szene als Elternknoten
+        rate: 8, // Erzeugungsrate der Partikel
+        texture: '../assets/img/smoke.png' // Textur für die Partikel
     }
 
+    // Partikelsystem für den definierten Kontext erstellen:
     const smokeEffect = getSmokeParticleSystem(smokeEffectContext);
 
     const animateEffect = () => {
 
+        // Effekt stoppen, wenn das Objekt nicht mehr im Spielfeld ist:
         if (!missileObjects.includes(object)) {
             smokeEffect.stop();
         }
         requestAnimationFrame(animateEffect);
+
+        // Partikelsystem aktualisieren, update():
+        // Fügt neue Partikel hinzu, basierend auf der definierten Rate und Emissionseinstellungen.
+        // Animiert bestehende Partikel, Position, Lebenszeit, Bewegung.
+        // Ändert die Geometrie, damit Änderungen im Partikelsystem visualisiert werden.
         smokeEffect.update(0.016);
     }
 
     animateEffect();
 }
 
+/**
+ * Explosionseffekt für ein Objekt erzeugen
+ * @param object: Objekt, an dem der Effekt angewendet wird
+ */
 function getExplosionEffect(object) {
 
+    // Kontext für das Partikelsystem definieren:
     let ExplosionEffectContext = {
-        camera,
-        emitter: object,
-        parent: scene,
-        rate: 20,
-        texture: '../assets/img/explosion.png'
+        camera, // Kamera, die die Partikel zeigt
+        emitter: object, // Objekt, das den Rauch ausstößt
+        parent: scene, // Szene als Elternknoten
+        rate: 20, // Erzeugungsrate der Partikel
+        texture: '../assets/img/explosion.png' // Textur für die Partikel
     }
 
+    // Partikelsystem für den definierten Kontext erstellen:
     const explosionEffect = getExplosionParticleSystem(ExplosionEffectContext);
 
     const animateEffect = () => {
 
+        // Effekt stoppen, wenn das Objekt nicht mehr im Spielfeld ist:
         if (spaceshipObj == null) {
             explosionEffect.stop();
         }
         requestAnimationFrame(animateEffect);
+
+        // Partikelsystem aktualisieren, update():
+        // Fügt neue Partikel hinzu, basierend auf der definierten Rate und Emissionseinstellungen.
+        // Animiert bestehende Partikel, Position, Lebenszeit, Bewegung.
+        // Ändert die Geometrie, damit Änderungen im Partikelsystem visualisiert werden.
         explosionEffect.update(0.016);
     }
 
     animateEffect();
 }
 
+/**
+ * Feuereffek für ein Objekt erzeugen
+ * @param object: Objekt, an dem der Effekt angewendet wird
+ */
 function getFireEffect(object) {
 
+    // Kontext für das Partikelsystem definieren:
     let FireEffectContext = {
-        camera,
-        emitter: object,
-        parent: scene,
-        rate: 70,
-        texture: '../assets/img/fire.png'
+        camera, // Kamera, die die Partikel zeigt
+        emitter: object, // Objekt, das den Rauch ausstößt
+        parent: scene, // Szene als Elternknoten
+        rate: 70, // Erzeugungsrate der Partikel
+        texture: '../assets/img/fire.png' // Textur für die Partikel
     }
 
     const fireEffect = getFireParticleSystem(FireEffectContext);
 
     const animateEffect = () => {
 
+        // Effekt stoppen, wenn das Objekt nicht mehr im Spielfeld ist:
         if (spaceshipObj == null) {
             fireEffect.stop();
         }
         requestAnimationFrame(animateEffect);
+
+        // Partikelsystem aktualisieren, update():
+        // Fügt neue Partikel hinzu, basierend auf der definierten Rate und Emissionseinstellungen.
+        // Animiert bestehende Partikel, Position, Lebenszeit, Bewegung.
+        // Ändert die Geometrie, damit Änderungen im Partikelsystem visualisiert werden.
         fireEffect.update(0.016);
     }
 
     animateEffect();
 }
 
+/**
+ * Lasereffek für ein Objekt erzeugen
+ * @param object: Objekt, an dem der Effekt angewendet wird
+ */
 function getLaserEffect(object) {
 
+    // Kontext für das Partikelsystem definieren:
     let laserEffectContext = {
-        camera,
-        emitter: object,
-        parent: scene,
-        rate: 20,
-        texture: '../assets/img/laser.png'
+        camera, // Kamera, die die Partikel zeigt
+        emitter: object, // Objekt, das den Rauch ausstößt
+        parent: scene, // Szene als Elternknoten
+        rate: 20, // Erzeugungsrate der Partikel
+        texture: '../assets/img/laser.png' // Textur für die Partikel
     }
 
     const laserEffect = getLaserParticleSystem(laserEffectContext);
 
     const animateEffect = () => {
 
+        // Effekt stoppen, wenn das Objekt nicht mehr im Spielfeld ist:
         if (spaceshipObj == null) {
             laserEffect.stop();
         }
         requestAnimationFrame(animateEffect);
+
+        // Partikelsystem aktualisieren, update():
+        // Fügt neue Partikel hinzu, basierend auf der definierten Rate und Emissionseinstellungen.
+        // Animiert bestehende Partikel, Position, Lebenszeit, Bewegung.
+        // Ändert die Geometrie, damit Änderungen im Partikelsystem visualisiert werden.
         laserEffect.update(0.016);
     }
 
